ffe5c5b08a4e23730771e77cf289998d87b025ed
Marcus Jun <36864968+marcusjun@users.noreply.github.com>
2018-05-20 15:38:10 -0700

    added constant variable of State codes

--- lib/weather_alert_mbj/cli.rb
+++ lib/weather_alert_mbj/cli.rb
@@ -1,8 +1,11 @@
 class WeatherAlertMbj::CLI
 
+  STATE_CODES = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+
   def call
     puts "Welcome to Weather Alert."
     get_state
+    goodbye
   end
 
   def get_state
@@ -10,14 +13,17 @@ class WeatherAlertMbj::CLI
     input = ""
 
     while input != "exit"
-      puts "Please enter the two-letter code for the state you wish to get weather alerts for."
-      puts "For example, CA = California and TX = Texas"
-      puts "Or type 'Exit' to exit."
+      puts "\nPlease enter the two-letter code for the state you wish to get weather alerts for."
+      puts "  For example, CA = California and TX = Texas"
+      puts "  Or type 'Exit' to exit."
 
       input = gets.strip.downcase
+
     end
+  end
 
-
+  def goodbye
+    puts "\nThanks for using Weather Alert. Goodbye."
   end
 
 end
