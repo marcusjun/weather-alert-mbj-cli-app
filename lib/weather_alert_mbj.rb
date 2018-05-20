f23c8a0c685333d480f5d2574b7b32acb1f273cc
Marcus Jun <36864968+marcusjun@users.noreply.github.com>
2018-05-20 16:27:42 -0700

    required relative for new class:
    WeatherAlertMbj::Alert

--- lib/weather_alert_mbj.rb
+++ lib/weather_alert_mbj.rb
@@ -3,8 +3,9 @@ require 'pry'
 require 'nokogiri'
 
 #require "weather_alert_mbj/version"
-require_relative "./weather_alert_mbj/version"
+require_relative "./weather_alert_mbj/alert"
 require_relative './weather_alert_mbj/cli'
+require_relative "./weather_alert_mbj/version"
 
 #module WeatherAlertMbj
   # Your code goes here...
