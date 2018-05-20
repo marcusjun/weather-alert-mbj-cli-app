4e2b9c3cb8e3c8f3d9e2277326bd0421af054f8e
Marcus Jun <36864968+marcusjun@users.noreply.github.com>
2018-05-20 15:09:11 -0700

    added while loop to get user input (state code) or exit

--- lib/weather_alert_mbj/cli.rb
+++ lib/weather_alert_mbj/cli.rb
@@ -6,9 +6,18 @@ class WeatherAlertMbj::CLI
   end
 
   def get_state
-    puts "Please enter the two-letter code for the state you wish to get weather alerts for."
-    puts "For example, CA = California and TX = Texas"
-    input = gets.strip
+
+    input = ""
+
+    while input != "exit"
+      puts "Please enter the two-letter code for the state you wish to get weather alerts for."
+      puts "For example, CA = California and TX = Texas"
+      puts "Or type 'Exit' to exit."
+
+      input = gets.strip.downcase
+    end
+
+
   end
 
 end
