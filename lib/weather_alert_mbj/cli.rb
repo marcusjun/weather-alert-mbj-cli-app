41ba51e5be7f092c79db95b1c068b0a4672553dd
Marcus Jun <36864968+marcusjun@users.noreply.github.com>
2018-05-20 15:03:25 -0700

    added #get_state which asks user for a state code
    Ex: CA = California

--- lib/weather_alert_mbj/cli.rb
+++ lib/weather_alert_mbj/cli.rb
@@ -4,4 +4,10 @@ class WeatherAlertMbj::CLI
     puts "Welcome to Weather Alert."
   end
 
+  def get_state
+    puts "Please enter the two-letter code for the state you wish to get weather alerts for."
+    puts "For example, CA = California and TX = Texas"
+    input = gets.strip
+  end
+
 end
