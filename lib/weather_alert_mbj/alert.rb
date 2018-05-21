a7e075623d290153898a6e620191567908ec7649
Marcus Jun <36864968+marcusjun@users.noreply.github.com>
2018-05-20 19:33:16 -0700

    added #webscraper method, preliminary
    still need to figure out how to create multiple instances of the class Alerts

--- lib/weather_alert_mbj/alert.rb
+++ lib/weather_alert_mbj/alert.rb
@@ -1,10 +1,39 @@
 class WeatherAlertMbj::Alert
 
-  attr_accessor :name, :issue_date, :expiration_date, :urgency, :status, :areas_affected, :url, :state
+  attr_accessor :name, :issue_date, :expiration_date, :urgency, :status, :areas_affected, :state_url, :state, :alert_url
+
+  @@all=[]
 
   def self.create_alerts(input)
+
+    #def self.create(name)
+      #new_artist=self.new(name)
+      #new_artist.save
+      #new_artist
+    #end
+
     @state = input
-    @url = "https://alerts.weather.gov/cap/#{@state}.php?x=1"
+    @state_url = "https://alerts.weather.gov/cap/#{@state}.php?x=1"
+    #web_scraper
   end
+
+  def web_scraper
+    #take the @url and scrape
+
+    doc = Nokogiri::HTML(open(@url))
+
+    doc.css(".entry").each do |box|
+      @name = box.css("a").text
+      if box.css(".label").text.contains?("Urgency:")
+        @urgency = box.css("span").text
+      end
+    end
+
+    #@@all << self.new
+
+  end
+
+
+
 
 end
