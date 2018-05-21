e041f4d331e7f3330d71d73f61ad5eb53b858271
Marcus Jun <36864968+marcusjun@users.noreply.github.com>
2018-05-20 19:16:10 -0700

    added #self.create_alerts(input) basic functionality
    it assigns the state and url

--- lib/weather_alert_mbj/alert.rb
+++ lib/weather_alert_mbj/alert.rb
@@ -1,3 +1,10 @@
 class WeatherAlertMbj::Alert
 
+  attr_accessor :name, :issue_date, :expiration_date, :urgency, :status, :areas_affected, :url, :state
+
+  def self.create_alerts(input)
+    @state = input
+    @url = "https://alerts.weather.gov/cap/#{@state}.php?x=1"
+  end
+
 end
