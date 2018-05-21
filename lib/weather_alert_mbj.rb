7cf86b4c4e9057f32ac6be7282aae533402c2ca9
Marcus Jun <36864968+marcusjun@users.noreply.github.com>
2018-05-20 17:08:26 -0700

    require_relative "./weather_alert_mbj/version" has to be 1st?

--- lib/weather_alert_mbj.rb
+++ lib/weather_alert_mbj.rb
@@ -3,9 +3,11 @@ require 'pry'
 require 'nokogiri'
 
 #require "weather_alert_mbj/version"
-require_relative "./weather_alert_mbj/alert"
-require_relative './weather_alert_mbj/cli'
 require_relative "./weather_alert_mbj/version"
+
+require_relative './weather_alert_mbj/alert'
+require_relative "./weather_alert_mbj/cli"
+
 
 #module WeatherAlertMbj
   # Your code goes here...
