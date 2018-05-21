60ae1f404eeac94c315431af7d470d5667770a5b
Marcus Jun <36864968+marcusjun@users.noreply.github.com>
2018-05-21 16:26:14 -0700

    working on scraping, moving away from temp placeholder
    instances of class Alert

--- lib/weather_alert_mbj/alert.rb
+++ lib/weather_alert_mbj/alert.rb
@@ -6,25 +6,26 @@ class WeatherAlertMbj::Alert
 
   def self.create_alerts(input)
 
-    alert_1 = self.new
-    alert_1.name = "Flood Warning"
-    alert_1.state = "ar"
-    alert_1.issue_date = "May 21 at 10:47AM CDT"
-    alert_1.status = "Actual"
-    alert_1.alert_url = "https://alerts.weather.gov/cap/wwacapget.php?x=AR125AA4CB788C.FloodWarning.125AA4D95B3CAR.LZKFLSLZK.7c002c3b990c5673652a38373a4a2842"
+    
+    #alert_1 = self.new
+    #alert_1.name = "Flood Warning"
+    #alert_1.state = "ar"
+    #alert_1.issue_date = "May 21 at 10:47AM CDT"
+    #alert_1.status = "Actual"
+    #alert_1.alert_url = "https://alerts.weather.gov/cap/wwacapget.php?x=AR125AA4CB788C.FloodWarning.125AA4D95B3CAR.LZKFLSLZK.7c002c3b990c5673652a38373a4a2842"
 
-    @@all << alert_1
+    #@@all << alert_1
 
-    alert_2 = self.new
-    alert_2.name = "Dense Fog Advisory"
-    alert_2.state = "wi"
-    alert_2.issue_date = "May 21 at 11:01AM CDT"
-    alert_2.status = "Actual"
-    alert_2.alert_url = "https://alerts.weather.gov/cap/wwacapget.php?x=WI125AA4CB8DA4.DenseFogAdvisory.125AA4DA3340WI.MKXNPWMKX.eca3d684369e7e89182de7826d073f28"
+    #alert_2 = self.new
+    #alert_2.name = "Dense Fog Advisory"
+    #alert_2.state = "wi"
+    #alert_2.issue_date = "May 21 at 11:01AM CDT"
+    #alert_2.status = "Actual"
+    #alert_2.alert_url = "https://alerts.weather.gov/cap/wwacapget.php?x=WI125AA4CB8DA4.DenseFogAdvisory.125AA4DA3340WI.MKXNPWMKX.eca3d684369e7e89182de7826d073f28"
 
-    @@all << alert_2
+    #@@all << alert_2
 
-    @@all
+    #@@all
 
     #[alert_1,alert_2]
 
@@ -42,10 +43,10 @@ class WeatherAlertMbj::Alert
     #web_scraper
   end
 
-  def web_scraper
+  def self.web_scraper
     #take the @url and scrape
 
-    doc = Nokogiri::HTML(open(@url))
+    doc = Nokogiri::HTML(open(@state_url))
 
     doc.css(".entry").each do |box|
       @name = box.css("a").text
