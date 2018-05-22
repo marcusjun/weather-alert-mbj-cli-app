71b493479fb5ce25f163de7ff5b3d36dabfd5fc4
Marcus Jun <36864968+marcusjun@users.noreply.github.com>
2018-05-22 16:04:40 -0700

    require_relative './weather_alert_mbj/alert'

--- lib/weather_alert_mbj.rb
+++ lib/weather_alert_mbj.rb
@@ -1,14 +1,13 @@
 require 'open-uri'
 require 'pry'
 require 'nokogiri'
+#require 'colorize'
 
 #require "weather_alert_mbj/version"
 require_relative "./weather_alert_mbj/version"
 
-require_relative './weather_alert_mbj/alert'
-require_relative "./weather_alert_mbj/cli"
-
-
 #module WeatherAlertMbj
   # Your code goes here...
 #end
+require_relative './weather_alert_mbj/alert'
+require_relative './weather_alert_mbj/cli'
