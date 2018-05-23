3c460c34a5f9654a6d5ee41c18e13610c3618f7b
Marcus Jun <36864968+marcusjun@users.noreply.github.com>
2018-05-23 14:42:27 -0700

    alert: working on getting name of state

--- lib/weather_alert_mbj/alert.rb
+++ lib/weather_alert_mbj/alert.rb
@@ -1,6 +1,6 @@
 class WeatherAlertMbj::Alert
 
-  attr_accessor :name, :date, :urgency, :status, :areas_affected, :state_url, :state_code, :state_name :alert_url, :description, :instructions
+  attr_accessor :name, :date, :urgency, :status, :areas_affected, :state_url, :state, :alert_url, :description, :instructions
 
   #@@all=[]
 
@@ -18,10 +18,10 @@ class WeatherAlertMbj::Alert
     #doc.css("event").each do |box|
     doc.css("entry").each do |box|
     #doc.css(".headline").each do |box|
-      binding.pry
+      #binding.pry
       new_alert = self.new
-      new_alert.state_code = input.upcase
-      new_alert.state_name = doc.css("title").text.gsub("Current Watches, Warnings and Advisories for ","").gsub(" Issued by the National Weather ServiceThere are no active watches, warnings or advisories","")
+      new_alert.state = input.upcase
+      #new_alert.state_name = doc.css("title").text.gsub("Current Watches, Warnings and Advisories for ","").gsub(" Issued by the National Weather ServiceThere are no active watches, warnings or advisories","")
       new_alert.state_url = url_holder
 
       #For states that currently do not have any active watches, warnings or advisories
